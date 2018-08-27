9baee99fbdb8f7e2210f61e43fe485205c20e759
xinxiaoyi <623928118@qq.com>
2018-08-27 09:02:05 +0000

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
 {
-    //
+    //用户信息展示页
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //用户信息编辑页
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //修改用户信息操作
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
